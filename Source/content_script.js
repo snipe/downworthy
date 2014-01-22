7da323dff6f6f640565779a8c68985e73ff0bc6b
snipe <snipe@snipe.net>
2014-01-22 01:16:26 -0500

    A few more text variations

--- Source/content_script.js
+++ Source/content_script.js
@@ -24,9 +24,9 @@ function walk(node)
 
 		case 3: // Text node
 			handleText(node);
-			console.log("Hello, world!")
 			break;
 	}
+	
 }
 
 function handleText(textNode) 
@@ -40,13 +40,21 @@ function handleText(textNode)
 	v = v.replace(/\bDoesn't want you to see\b/g, "Doesn't Really Care If You See");
 	v = v.replace(/\bOne Weird Trick\b/g, "One Piece of Completely Anecdotal Horsehit");
 	v = v.replace(/\bYou Wont Believe\b/g, "In All Likelihood, You'll Believe");
+	v = v.replace(/\bYou Won&#8217;t Believe\b/g, "In All Likelihood, You'll Believe");
+	v = v.replace(/\bYou Won’t Believe\b/g, "In All Likelihood, You'll Believe");	
 	v = v.replace(/\bYou Won't Believe\b/g, "In All Likelihood, You'll Believe");
+	v = v.replace(/\bCan't Handle\b/g, "Can Totally Handle");
+	v = v.replace(/\bcan't handle\b/g, "can totally handle");
 	v = v.replace(/\bCan't Even Handle\b/g, "Can Totally Handle");
 	v = v.replace(/\bCannot Even Handle\b/g, "Can Probably Totally Handle");
 	v = v.replace(/\bcannot even handle\b/g, "Can Probably Totally Handle");
 	v = v.replace(/\bcan't even handle\b/g, "can totally probably handle");
 	v = v.replace(/\bWon the Internet\b/g, "Seemed Pretty Cool");
 	v = v.replace(/\bwon the internet\b/g, "seemed pretty cool");
+	v = v.replace(/\bScientific Reasons\b/g, "Vaguely Science-y Reasons");
+	v = v.replace(/\bThis Is What Happens\b/g, "This Is Our Bullshit Clickbait Version Of What Happens");
+	v = v.replace(/\bOne Weird Trick\b/g, "One Piece of Nonsensical Folklore Bullshit ");
+	
 	
 	textNode.nodeValue = v;
 }
